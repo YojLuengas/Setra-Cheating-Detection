0e0a01a25dfebaf562bf782ca4e0ec4d52f0ed47
Jomar Lorigas <jomarlorigas@gmail.com>
2025-07-22 20:49:23 +0800

    Updated ni

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, Response
+import cv2
+
+app = Flask(__name__)
+camera = cv2.VideoCapture(0)  # 0 = default webcam
+
+def generate_frames():
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            # Convert frame to JPEG
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+
+            # Yield the frame in HTTP response format
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/')
+def index():
+    return render_template('HomePage.html')  # loads your SENTRA HTML
+
+@app.route('/video')
+def video():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
